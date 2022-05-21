db7dea369b7ad885e73ef5ca7868c844003e2d94
Ewa Domańska <ewadomanska0510@gmail.com>
2022-05-21 11:35:08 +0200

    2 task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
+
+for (const ingredient of ingredients) {
+    const vege = document.createElement("li");
+    vege.textContent = ingredient
+    vege.className = "item"
+    const veges = document.getElementsByTagName("ul")[0]
+    veges.append(vege)
+}

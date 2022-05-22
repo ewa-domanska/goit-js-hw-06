2e2078a801dd43af2e846feb9bbd461d306c29c3
Ewa Domańska <ewadomanska0510@gmail.com>
2022-05-22 19:44:19 +0200

    Part of 8 task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (e) => {
+    const {
+        elements: {email, password},
+    } = e.currentTarget;
+
+    if (email.value.length === 0 || password.value.length === 0) {
+    alert("Wszystkie pola powinny zostać wypełnione.")
+    }
+
+    console.log(email.value)
+    console.log(password.value)
+});
 

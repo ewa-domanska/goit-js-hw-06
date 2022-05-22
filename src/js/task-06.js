faead26c1bb4cf3bc8b60cbca4e0dadab39d09d8
Ewa Domańska <ewadomanska0510@gmail.com>
2022-05-22 10:22:33 +0200

    6 task

---
+++ src/js/task-06.js
@@ -0,0 +1,11 @@
+let element = document.querySelector("#validation-input");
+let minLength = element.dataset.length;
+
+
+element.addEventListener("blur", () => {
+    if (element.value.length > minLength) {
+        element.className = "valid";
+    } else {
+        element.className = "invalid";
+    }
+});

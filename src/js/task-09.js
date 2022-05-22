f9a8d8c44a2e9bcde0eaae78162ab8dcd8e86c78
Ewa Domańska <ewadomanska0510@gmail.com>
2022-05-22 11:11:22 +0200

    9 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const button = document.querySelector(".change-color")
+let body = document.querySelector("body")
+let span = document.querySelector(".color")
+
+button.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  body.setAttribute("style", "background-color:" + color);
+  span.textContent = color;
+});
